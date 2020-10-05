225645daaea82f773ed44d8f73aaa7f3225c1632
k.maeda <k.maeda@systemi.co.jp>
2020-10-05 15:01:13 +0900

    'fix'

--- Webpack_HTML_CSS_JS/section11/src/javascripts/my.js
+++ Webpack_HTML_CSS_JS/section11/src/javascripts/my.js
@@ -1,6 +1,6 @@
 // 自作モジュールのデモ
 export default () => {
   const obj = { a: 1, b: 2 };
-  const newObj = { ...obj, c: 3 };
+  const newObj = { ...obj, c: 3 }; // {a: 1, b: 2, c: 3}
   console.log(`this is module`, newObj);
 };
